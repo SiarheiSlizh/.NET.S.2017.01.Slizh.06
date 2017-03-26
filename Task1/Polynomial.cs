e4592b0211503ce316a36f89fbf19c36953301c8
сергей слиж <sergeislizh@mail.ru>
2017-03-27 02:31:52 +0300

    add Task1

---
+++ Task1/Polynomial.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class Polynomial
+    {
+        #region fields
+        private int[] arrCoeff;
+        private double x;
+        #endregion
+
+        #region Constructors
+        public Polynomial(int coeff1)
+        {
+            arrCoeff = new int[] { coeff1 };
+        }
+
+        public Polynomial(int coeff1, int coeff2)
+        {
+            arrCoeff = new int[] { coeff1, coeff2 };
+        }
+
+        public Polynomial(int coeff1, int coeff2, int coeff3)
+        {
+            arrCoeff = new int[] { coeff1, coeff2, coeff3 };
+        }
+
+        public Polynomial(params int[] arr)
+        {
+            arrCoeff = new int[arr.Length];
+            Array.Copy(arr, arrCoeff, arr.Length);
+        }
+        #endregion
+
+        #region Object override
+        public override string ToString()
+        {
+            int kol = arrCoeff.Length;
+            StringBuilder result = new StringBuilder("P(x) = ");
+
+            for (int i = 0; i < arrCoeff.Length - 1; i++)
+                result.Append($"{arrCoeff[i]}x^{--kol} + ");
+            result.Append(arrCoeff.Last());
+
+            return result.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Polynomial) || ((Polynomial)obj).GetHashCode() != GetHashCode())
+                return false;
+
+            for (int i = 0; i < arrCoeff.Length; i++)
+                if (arrCoeff[i] != ((Polynomial)obj).arrCoeff[i])
+                    return false;
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            return arrCoeff.Length;
+        }
+        #endregion
+
+        #region operator overloading
+        public static Polynomial operator + (Polynomial p1, Polynomial p2) => Add(p1,p2);
+        public static Polynomial operator - (Polynomial p1, Polynomial p2) => Sub(p1,p2);
+        public static Polynomial operator * (Polynomial p1, Polynomial p2) => Mul(p1,p2);
+        public static bool operator == (Polynomial p1, Polynomial p2) => Eql(p1,p2);
+        public static bool operator != (Polynomial p1, Polynomial p2) => NotEql(p1,p2);
+        #endregion
+
+        #region private operator overloading
+        private static Polynomial Add(Polynomial p1, Polynomial p2)
+        {
+            if (p1 == null || p2 == null)
+                throw new ArgumentException();
+
+            int maxSize = p1.arrCoeff.Length;
+            if (p1.arrCoeff.Length < p2.arrCoeff.Length)
+                maxSize = p2.arrCoeff.Length;
+            
+            Polynomial p = new Polynomial { arrCoeff = new int[maxSize] };
+            Array.Copy(p1.arrCoeff, 0, p.arrCoeff, maxSize - p1.arrCoeff.Length, p1.arrCoeff.Length);
+            for (int i = maxSize - p2.arrCoeff.Length; i < maxSize; i++)
+                p.arrCoeff[i] += p2.arrCoeff[i];
+
+            return p;
+        }
+
+        private static Polynomial Sub(Polynomial p1, Polynomial p2)
+        {
+            if (p1 == null || p2 == null)
+                throw new ArgumentException();
+
+            int maxSize = p1.arrCoeff.Length;
+            if (p1.arrCoeff.Length < p2.arrCoeff.Length)
+                maxSize = p2.arrCoeff.Length;
+
+            Polynomial p = new Polynomial { arrCoeff = new int[maxSize] };
+            Array.Copy(p1.arrCoeff, 0, p.arrCoeff, maxSize - p1.arrCoeff.Length, p1.arrCoeff.Length);
+            for (int i = maxSize - p2.arrCoeff.Length; i < maxSize; i++)
+                p.arrCoeff[i] -= p2.arrCoeff[i];
+
+            return p;
+        }
+
+        private static Polynomial Mul(Polynomial p1, Polynomial p2)
+        {
+            if (p1 == null || p2 == null)
+                throw new ArgumentException();
+
+            Polynomial p = new Polynomial { arrCoeff = new int[p1.arrCoeff.Length + p2.arrCoeff.Length - 1] };
+            for (int i = 0; i < p1.arrCoeff.Length; i++)
+                for (int j = 0; j < p2.arrCoeff.Length; j++)
+                    p.arrCoeff[i + j] += p1.arrCoeff[i] * p2.arrCoeff[j];
+
+            return p;
+        }
+
+        private static bool Eql(Polynomial p1, Polynomial p2)
+        {
+            if (ReferenceEquals(p1, p2)) return true;
+            else return false;
+        }
+
+        private static bool NotEql(Polynomial p1, Polynomial p2)
+        {
+            if (!ReferenceEquals(p1, p2)) return true;
+            else return false;
+        }
+        #endregion
+    }
+}
---
+++ Task1Tests/PolynomialTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+
+namespace Task1Tests
+{
+    [TestFixture]
+    public class PolynomialTests
+    {
+        [TestCase(new int[] { 3, 4, 6 }, ExpectedResult = "P(x) = 3x^2 + 4x^1 + 6")]
+        [TestCase(new int[] { 6, 6, 18, 5, -7 }, ExpectedResult = "P(x) = 6x^4 + 6x^3 + 18x^2 + 5x^1 + -7")]
+        public string ToString_PositiveTests(int[] args)
+        {
+            return new Polynomial(args).ToString();
+        }
+
+        [TestCase(new int[] { 3, 4, 6 }, new int[] { 6, 6, 18, 5, -7 }, ExpectedResult = false)]
+        [TestCase(new int[] { 3, 4, 6 }, new int[] { 3, 4, 6 }, ExpectedResult = true)]
+        [TestCase(new int[] { 3, 4, 6 }, new int[] { }, ExpectedResult = false)]
+        public bool Equals_PositiveTests(int[] args1, int[] args2)
+        {
+            return new Polynomial(args1).Equals(new Polynomial(args2));
+        }
+
+        [TestCase(new int[] { 3, 4, 6}, ExpectedResult = 3)]
+        [TestCase(new int[] { }, ExpectedResult = 0)]
+        public int GetHashCode_PositiveTests(int[] args)
+        {
+            return new Polynomial(args).GetHashCode();
+        }
+
+        [TestCase(new int[] { 3, 4, 6 }, new int[] { 6, 6, 18, 5, -7 }, ExpectedResult = "P(x) = 6x^4 + 6x^3 + 21x^2 + 9x^1 + -1")]
+        [TestCase(new int[] { 3, 4, 6 }, new int[] { 3, 4, 6 }, ExpectedResult = "P(x) = 6x^2 + 8x^1 + 12")]
+        public string Add_PositiveTests(int[] args1, int[] args2)
+        {
+            return (new Polynomial(args1) + new Polynomial(args2)).ToString();
+        }
+
+        [TestCase(new int[] { 3, 4, 6 }, new int[] { 6, 6, 18, 5, -7 }, ExpectedResult = "P(x) = -6x^4 + -6x^3 + -15x^2 + -1x^1 + 1")]
+        [TestCase(new int[] { 6, -4, 9 }, new int[] { 3, 4, 6 }, ExpectedResult = "P(x) = 3x^2 + -8x^1 + 3")]
+        public string Sub_PositiveTests(int[] args1, int[] args2)
+        {
+            return (new Polynomial(args1) - new Polynomial(args2)).ToString();
+        }
+
+        [TestCase(new int[] { 5, -8, 2 }, new int[] { 2, 3 }, ExpectedResult = "P(x) = 10x^3 + -1x^2 + -20x^1 + 6")]
+        [TestCase(new int[] { 3, 2, 2 }, new int[] { 4, 3, 0, 5, 0 }, ExpectedResult = "P(x) = 12x^6 + 17x^5 + 14x^4 + 21x^3 + 10x^2 + 10x^1 + 0")]
+        public string Mul_PositiveTests(int[] args1, int[] args2)
+        {
+            return (new Polynomial(args1) * new Polynomial(args2)).ToString();
+        }
+
+        [TestCase(null, ExpectedResult = false)]
+        public bool Eql_PositiveTests(Polynomial p2)
+        {
+            return new Polynomial(3, 4) == p2;
+        }
+
+        [TestCase(null, ExpectedResult = true)]
+        public bool NotEql_PositiveTests(Polynomial p2)
+        {
+            return new Polynomial(3, 4) != p2;
+        }
+    }
+}
