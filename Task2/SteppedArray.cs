8da0bba4f86ca0fce410334f32caf01fc310be0c
сергей слиж <sergeislizh@mail.ru>
2017-03-27 19:10:45 +0300

    add Task2 + XML comments

--- Task2/SteppedArray.cs
+++ Task2/SteppedArray.cs
@@ -6,8 +6,15 @@ using System.Threading.Tasks;
 
 namespace Task2
 {
+    /// <summary>
+    /// This class works with stepped arrays.
+    /// </summary>
     public static class SteppedArray
     {
+        /// <summary>
+        /// Method sorts stepped array in order by sum of row's elements ascending.
+        /// </summary>
+        /// <param name="arr">Stepped array.</param>
         public static void BubbleSortOrderBySumRowsAsc(int[][] arr)
         {
             ArgExcecption(arr);
@@ -17,6 +24,10 @@ namespace Task2
                         Replace(ref arr[i], ref arr[j]);
         }
 
+        /// <summary>
+        /// Method sorts stepped array in order by sum of row's elements descending.
+        /// </summary>
+        /// <param name="arr">Stepped array.</param>
         public static void BubbleSortOrderBySumRowsDesc(int[][] arr)
         {
             ArgExcecption(arr);
@@ -24,6 +35,10 @@ namespace Task2
             arr.Reverse();
         }
 
+        /// <summary>
+        /// Method sorts stepped array in order by max elemets in row ascending.
+        /// </summary>
+        /// <param name="arr">Stepped array.</param>
         public static void BubbleSortOrderByMaxElemInRowAsc(int[][] arr)
         {
             ArgExcecption(arr);
@@ -33,14 +48,21 @@ namespace Task2
                         Replace(ref arr[i], ref arr[j]);
         }
 
+        /// <summary>
+        /// Method sorts stepped array in order by max elemets in row descending.
+        /// </summary>
+        /// <param name="arr">Stepped array.</param>
         public static void BubbleSortOrderByMaxElemInRowDesc(int[][] arr)
         {
             ArgExcecption(arr);
             BubbleSortOrderByMaxElemInRowAsc(arr);
             arr.Reverse();
         }
-        
 
+        /// <summary>
+        /// Method sorts stepped array in order by min elemets in row ascending.
+        /// </summary>
+        /// <param name="arr">Stepped array.</param>
         public static void BubbleSortOrderByMinElemInRowAsc(int[][] arr)
         {
             ArgExcecption(arr);
@@ -50,6 +72,10 @@ namespace Task2
                         Replace(ref arr[i], ref arr[j]);
         }
 
+        /// <summary>
+        /// Method sorts stepped array in order by max elemets in row descending.
+        /// </summary>
+        /// <param name="arr">Stepped array.</param>
         public static void BubbleSortOrderByMinElemInRowDesc(int[][] arr)
         {
             ArgExcecption(arr);
@@ -57,6 +83,10 @@ namespace Task2
             arr.Reverse();
         }
 
+        /// <summary>
+        /// Method checks data on validity.
+        /// </summary>
+        /// <param name="arr">Stepped array.</param>
         private static void ArgExcecption(int[][] arr)
         {
             if (arr == null)
@@ -66,6 +96,11 @@ namespace Task2
                     throw new ArgumentException();
         }
 
+        /// <summary>
+        /// Method replaces two array's references.
+        /// </summary>
+        /// <param name="arr1">Dimensional array.</param>
+        /// <param name="arr2">Dimensional array.</param>
         private static void Replace(ref int[] arr1, ref int[] arr2)
         {
             int[] temp = arr1;
