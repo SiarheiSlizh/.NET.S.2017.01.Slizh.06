967ba542a58159893bd9b06c3654cc5ef77eb547
сергей слиж <sergeislizh@mail.ru>
2017-03-27 11:04:21 +0300

    addTask1 + XML

--- Task1/Polynomial.cs
+++ Task1/Polynomial.cs
@@ -6,29 +6,54 @@ using System.Threading.Tasks;
 
 namespace Task1
 {
+    /// <summary>
+    /// This class works with polynomials.
+    /// </summary>
     public class Polynomial
     {
+        
         #region fields
+        /// <summary>
+        /// Coeffitients of polynomial.
+        /// </summary>
         private int[] arrCoeff;
-        private double x;
         #endregion
 
         #region Constructors
+        /// <summary>
+        /// This constructor allaws to initialize Poynomial of 0 degree.
+        /// </summary>
+        /// <param name="coeff1">Coeffitient.</param>
         public Polynomial(int coeff1)
         {
             arrCoeff = new int[] { coeff1 };
         }
 
+        /// <summary>
+        /// This constructor allaws to initialize Poynomial of 1 degree.
+        /// </summary>
+        /// <param name="coeff1">Coeffitient of 1 degree.</param>
+        /// <param name="coeff2">Coeffitient of 0 degree.</param>
         public Polynomial(int coeff1, int coeff2)
         {
             arrCoeff = new int[] { coeff1, coeff2 };
         }
 
+        /// <summary>
+        /// This constructor allaws to initialize Poynomial of 2 degree.
+        /// </summary>
+        /// <param name="coeff1">Coeffitient of 2 degree.</param>
+        /// <param name="coeff2">Coeffitient of 1 degree.</param>
+        /// <param name="coeff3">Coeffitient of 0 degree.</param>
         public Polynomial(int coeff1, int coeff2, int coeff3)
         {
             arrCoeff = new int[] { coeff1, coeff2, coeff3 };
         }
 
+        /// <summary>
+        /// This constructor allaws to initialize Poynomial of n degree.
+        /// </summary>
+        /// <param name="arr">Array of coeffitients of polynomial of n degree.</param>
         public Polynomial(params int[] arr)
         {
             arrCoeff = new int[arr.Length];
@@ -37,6 +62,10 @@ namespace Task1
         #endregion
 
         #region Object override
+        /// <summary>
+        /// This method writes the Polynomial.
+        /// </summary>
+        /// <returns>Polynomial which is transformed into string.</returns>
         public override string ToString()
         {
             int kol = arrCoeff.Length;
@@ -49,6 +78,11 @@ namespace Task1
             return result.ToString();
         }
 
+        /// <summary>
+        /// This Method compares internal conditions in two polynomials.
+        /// </summary>
+        /// <param name="obj">Object type of polynomial.</param>
+        /// <returns>True in case of equality of two polynomials.</returns>
         public override bool Equals(object obj)
         {
             if (obj == null || !(obj is Polynomial) || ((Polynomial)obj).GetHashCode() != GetHashCode())
@@ -61,17 +95,55 @@ namespace Task1
             return true;
         }
 
+        /// <summary>
+        /// This method calculates hash of object type of Polynomial.
+        /// </summary>
+        /// <returns>Length of array of coeffitients.</returns>
         public override int GetHashCode()
         {
             return arrCoeff.Length;
         }
         #endregion
 
-        #region operator overloading
+        #region overloading operations
+        /// <summary>
+        /// Overloding operation which allaws to find addition betwrrn two objects type of polynomial.
+        /// </summary>
+        /// <param name="p1">Object type of polynomial.</param>
+        /// <param name="p2">Object type of polynomial.</param>
+        /// <returns>New object type of polynomial.</returns>
         public static Polynomial operator + (Polynomial p1, Polynomial p2) => Add(p1,p2);
+
+        /// <summary>
+        /// Overloding operation which allaws to find substraction between two objects type of polynomial.
+        /// </summary>
+        /// <param name="p1">Object type of polynomial.</param>
+        /// <param name="p2">Object type of polynomial.</param>
+        /// <returns>New object type of polynomial.</returns>
         public static Polynomial operator - (Polynomial p1, Polynomial p2) => Sub(p1,p2);
+
+        /// <summary>
+        /// Overloding operation which allaws to find multiply between two objects type of polynomial.
+        /// </summary>
+        /// <param name="p1">Object type of polynomial</param>
+        /// <param name="p2">Object type of polynomial</param>
+        /// <returns>New object type of polynomial.</returns>
         public static Polynomial operator * (Polynomial p1, Polynomial p2) => Mul(p1,p2);
+
+        /// <summary>
+        /// Overloding operation which allaws to equal two objects type of polynomial.
+        /// </summary>
+        /// <param name="p1">Object type of polynomial</param>
+        /// <param name="p2">Object type of polynomial</param>
+        /// <returns>New object type of polynomial.</returns>
         public static bool operator == (Polynomial p1, Polynomial p2) => Eql(p1,p2);
+
+        /// <summary>
+        /// Overloding operation which allaws to equal two objects type of polynomial.
+        /// </summary>
+        /// <param name="p1">Object type of polynomial</param>
+        /// <param name="p2">Object type of polynomial</param>
+        /// <returns>New object type of polynomial.</returns>
         public static bool operator != (Polynomial p1, Polynomial p2) => NotEql(p1,p2);
         #endregion
 
